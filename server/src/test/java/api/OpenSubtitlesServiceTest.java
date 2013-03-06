c55c5538d344c5589a4bbfdd8ab3fe44a12f4f3f
kzimnick <krzysztof@zimnicki.biz>
2013-03-06 19:41:45 +0100

    Old changes before problem with computer. part 4

--- server/src/test/java/api/OpenSubtitlesServiceTest.java
+++ server/src/test/java/api/OpenSubtitlesServiceTest.java
@@ -1,9 +1,13 @@
 package api;
 
 import cc.explain.server.api.HashData;
-import cc.explain.server.core.OpenSubtitlesHasher;
+import cc.explain.server.api.OpenSubtitlesService;
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
 import java.util.List;
@@ -15,20 +19,92 @@ import static junit.framework.Assert.assertEquals;
  * Date: 15.11.12
  * Time: 13:16
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+        "classpath:spring-context.xml",
+        "classpath:spring-dao.xml",
+        "classpath:test-spring-dataSource.xml",
+        "classpath:spring-security.xml",
+        "classpath:spring-tx.xml"
+})
 public class OpenSubtitlesServiceTest {
+
+    @Autowired
+    OpenSubtitlesService openSubtitlesService;
 
     @Test
     public void shouldReturnCorrectHash() throws IOException {
-
         HashData data = new HashData();
         List<String> lines = IOUtils.readLines(Thread.currentThread().getContextClassLoader().getResourceAsStream("HashData.txt"));
         data.setSize(lines.get(0).split("=")[1]);
         data.setHead(lines.get(1));
         data.setTail(lines.get(2));
 
-        String hash = OpenSubtitlesHasher.computeHash(data);
+        String hash = openSubtitlesService.calculateHash(data);
 
-        assertEquals("f131627b23315325", hash);
-
+        assertEquals("8e245d9679d31e12", hash);
     }
+
+    @Test
+    public void shouldFindCorrectMovie() {
+        String hash = "80964db9ce3c9e6e"; //Madagascar 3
+        String size = "742848512";
+
+        openSubtitlesService.logInAnnonymously();
+        String idSubtitleFile = openSubtitlesService.searchMovieByHash(hash, size);
+
+        assertEquals("1953225164", idSubtitleFile);
+    }
+
+    @Test
+    public void shouldFindCorrectMovieByFilename() {
+        String fileName = "Madagascar.3.Europes.Most.Wanted.2012.DVDRip.XViD-PLAYNOW.avi"; //Madagascar 3
+
+        openSubtitlesService.logInAnnonymously();
+        String idSubtitleFile = openSubtitlesService.searchMovieByFilename(fileName);
+
+        assertEquals("1953225164", idSubtitleFile);
+    }
+
+    @Test
+    public void shouldNotFindCorrectMovieByFilename() {
+        String fileName = "badMovieFilename"; //Madagascar 3
+
+        openSubtitlesService.logInAnnonymously();
+        String idSubtitleFile = openSubtitlesService.searchMovieByFilename(fileName);
+
+        assertEquals("", idSubtitleFile);
+    }
+
+    @Test
+    public void shouldLoginUserInApiService() {
+        String login = "";
+        String password = "";
+        String userAgent = OpenSubtitlesService.OS_TEST_USER_AGENT;
+
+        boolean result = openSubtitlesService.logIn(login, password, userAgent);
+
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void shouldDownloadCorrectSubtitles() {
+        String subtitleFileId = "1953225164";
+
+        openSubtitlesService.logInAnnonymously();
+        String base64Subtitle = openSubtitlesService.downloadSubtitlesBase64GZIP(subtitleFileId);
+
+        assertEquals("H4sIAAAAAAAAA429zZLj", base64Subtitle.substring(0, 20));
+    }
+
+    @Test
+    public void shouldDecodeBase64GZIP() {
+        String base64GZIP = "H4sIAAAAAAAAAyvOz00tycjMSwcA+zHaCQkAAAA=";
+
+        String result = openSubtitlesService.decodeBase64GZIP(base64GZIP);
+
+        assertEquals("something", result);
+    }
+
+
 }
--- server/src/test/java/api/XmlRpcClientTest.java
+++ server/src/test/java/api/XmlRpcClientTest.java
@@ -6,8 +6,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.StringUtils;
 
 import java.io.IOException;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * User: kzimnick
@@ -21,6 +25,10 @@ public class XmlRpcClientTest {
     @Test
     public void test() throws IOException, XmlRpcException {
         XmlRpcService xmlRpcService = new XmlRpcService();
-        xmlRpcService.doTest("","");
+        String s = xmlRpcService.doTest("Anger Management [1x07] Charlie's Patient Gets Out of Jail.avi", "", "");
+
+        assertTrue(StringUtils.hasText(s));
     }
+
+
 }
