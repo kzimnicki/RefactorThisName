f2bc4d9f2f3e421585801fc4670e4deb3026b91a
kzimnick <krzysztof@zimnicki.biz>
2013-05-11 17:39:25 +0200

    Added translate service

--- server/src/test/java/cc/explain/server/api/TranslateServiceTest.java
+++ server/src/test/java/cc/explain/server/api/TranslateServiceTest.java
@@ -1,6 +1,12 @@
 package cc.explain.server.api;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
 
 import static junit.framework.Assert.assertEquals;
 
@@ -9,15 +15,26 @@ import static junit.framework.Assert.assertEquals;
  * Date: 27.04.13
  * Time: 18:26
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+        "classpath:spring-context.xml",
+        "classpath:spring-dao.xml",
+        "classpath:test-spring-dataSource.xml",
+        "classpath:spring-security.xml",
+        "classpath:spring-tx.xml"
+})
 public class TranslateServiceTest {
+
+   @Autowired
+   TranslateService translateService;
 
     @Test
     public void shouldTranslateEnglishWordToPolish(){
         String english = "get up";
 
-        String translated = new TranslateService().translate(english);
+        String[] translated = new TranslateService().translate(new String[]{english});
 
-        assertEquals("wstać", translated);
+        assertEquals("wstać", translated[0]);
     }
 
         @Test
@@ -29,6 +46,38 @@ public class TranslateServiceTest {
 
         assertEquals("wstać", translated[0]);
         assertEquals("schodzić", translated[1]);
+    }
+
+    @Test
+    public void shouldReturnTranslatedWord() throws Exception {
+           String word = "doghouse";
+
+           String translatedWord = translateService.getTranslatedWord(word);
+
+           assertEquals("psia buda", translatedWord);
+    }
+
+        @Test
+        public void shouldReturnTranslatedWordWithCorrectEncoding() throws Exception {
+           String word = "thickness";
+
+           String databaseTranslated = translateService.getTranslatedWord(word);
+            String[] googleTranslated = translateService.translate(new String[]{word});
+
+            print(databaseTranslated.getBytes());
+            print(googleTranslated[0].getBytes());
+            System.out.println(System.getProperty("file.encoding"));
+
+
+            assertEquals(databaseTranslated, googleTranslated[0]);
+    }
+
+    private void print(byte[] bytes){
+        for (byte b : bytes){
+            System.out.print(b);
+        }
+        System.out.println();
+
     }
 
 }
