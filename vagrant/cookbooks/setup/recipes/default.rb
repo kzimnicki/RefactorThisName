33f09546aa00078b0f3cc2295a6b334490bf700a
kzimnick <krzysztof@zimnicki.biz>
2013-06-01 22:43:36 +0200

    Added vagrant and sql scripts

--- vagrant/cookbooks/setup/recipes/default.rb
+++ vagrant/cookbooks/setup/recipes/default.rb
@@ -2,7 +2,8 @@ bash "import sql" do
 	code <<-EOH
 		service iptables stop
 		sudo chkconfig iptables off
-		mysql -u root -p123456 -e "create database if not exists englishtranslator"
 		mysql -u root -p123456 -e "create database if not exists testenglishtranslator"
+		mysql -u root -p123456 --force --default-character-set=utf8 testenglishtranslator < /vagrant/sql/ddl_explain_cc_database.sql
+		mysql -u root -p123456 --default-character-set=utf8 testenglishtranslator < /vagrant/sql/import.sql
 	EOH
 end
