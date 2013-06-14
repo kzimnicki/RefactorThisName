c096b891c2ff5043df869844ac91f594440d4db2
kzimnick <krzysztof@zimnicki.biz>
2013-06-14 20:38:45 +0200

    Updated testing https.

--- xbmc/xbmc.python.explaincc-1.0.0/test.py
+++ xbmc/xbmc.python.explaincc-1.0.0/test.py
@@ -5,17 +5,27 @@ import base64
 
 
 def getBasicAuthentication() :
-     auth = base64.encodestring('%s:%s' % ("user@gmail.com", "123456")).replace('\n','')
+     auth = base64.encodestring('%s:%s' % ("krzysztof@zimnicki.biz", "123456")).replace('\n','')
      return 'Basic %s' % auth
 
 
 params = urllib.urlencode({
-    'subtitle': "1234"
+    'subtitle': """1
+00:00:00,858 --> 00:00:03,006
+<i>Previously</i>
+<i>on</i> The Big Bang Theory...
+
+54
+00:02:54,928 --> 00:02:58,263
+who, like me,
+have been kept down by The Man."""
 })
 
-request = urllib2.Request('http://localhost:8080/RefactorThisName/app/quickSubtitleTranslate')
-request.add_header("Authorization",getBasicAuthentication())
-urllib2.urlopen(request, params)
+request = urllib2.Request('https://explain.cc/app/quickSubtitleTranslate')
+print getBasicAuthentication()
+#request.add_header("Authorization",getBasicAuthentication())
+response = urllib2.urlopen(request, params)
+print response.getcode()
 
 
   
