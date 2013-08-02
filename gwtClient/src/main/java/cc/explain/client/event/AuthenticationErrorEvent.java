ed9029d00ae0d1b6fd2999319e1e08039f08e0cc
kzimnick <krzysztof@zimnicki.biz>
2013-08-02 12:52:23 +0200

    Updated gitIgnore - changed slash to unix style

---
+++ gwtClient/src/main/java/cc/explain/client/event/AuthenticationErrorEvent.java
@@ -0,0 +1,23 @@
+package cc.explain.client.event;
+
+import com.google.gwt.event.shared.GwtEvent;
+
+/**
+ * User: kzimnick
+ * Date: 04.11.12
+ * Time: 14:13
+ */
+public class AuthenticationErrorEvent extends GwtEvent<UserLoggedOutEventHandler> {
+
+    public static Type<UserLoggedOutEventHandler> TYPE = new Type<UserLoggedOutEventHandler>();
+
+    @Override
+    public Type<UserLoggedOutEventHandler> getAssociatedType() {
+        return TYPE;
+    }
+
+    @Override
+    protected void dispatch(UserLoggedOutEventHandler handler) {
+        handler.onUserLoggedOutEvent();
+    }
+}
---
+++ gwtClient/src/main/java/cc/explain/client/event/AuthenticationErrorEventHandler.java
@@ -0,0 +1,15 @@
+package cc.explain.client.event;
+
+
+import com.google.gwt.event.shared.EventHandler;
+
+/**
+ * User: kzimnick
+ * Date: 04.11.12
+ * Time: 15:40
+ */
+public interface AuthenticationErrorEventHandler extends EventHandler {
+
+    public void onUserLoggedOutEvent();
+
+}
