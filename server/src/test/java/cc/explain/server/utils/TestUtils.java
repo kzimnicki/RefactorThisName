80e903f048d20abeed8c2b742b022082c55ec578
kzimnick <krzysztof@zimnicki.biz>
2013-08-02 19:37:30 +0200

    Fixing jenkins problem

--- server/src/test/java/cc/explain/server/utils/TestUtils.java
+++ server/src/test/java/cc/explain/server/utils/TestUtils.java
@@ -3,10 +3,7 @@ package cc.explain.server.utils;
 import org.springframework.mock.jndi.SimpleNamingContextBuilder;
 
 import javax.naming.NamingException;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Properties;
 
 /**
@@ -19,6 +16,7 @@ public class TestUtils {
     private TestUtils(){}
 
     public static void initTestJNDIFromPropertiesFile() throws IOException, NamingException {
+        System.out.println(new File(".").getAbsolutePath());
         SimpleNamingContextBuilder builder = new SimpleNamingContextBuilder();
         Properties prop = new Properties();
         InputStream stream = new FileInputStream("config/jndi.properties");
